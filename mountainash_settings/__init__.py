7a90dced3bc71b924711c49e79b9f4893ca2f915
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-03-19 22:06:06 +1100

    added githuib commit action

--- mountainash_settings/__init__.py
+++ mountainash_settings/__init__.py
@@ -3,5 +3,6 @@ from .version import __version__
 
 from .settings import SettingsParameters
 from .auth_settings import AuthSettings, get_auth_settings
+from .settings.settings_utils import SettingsUtils
 
-__all__ = ["SettingsParameters", "AuthSettings", "get_auth_settings"]
+__all__ = ["SettingsParameters", "AuthSettings", "get_auth_settings", "SettingsUtils"]
