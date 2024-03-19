cd66fea1183e1353928ed2ab4d9f54cf1bda8cfe
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-03-19 18:32:47 +1100

    updating references to util packages

--- mountainash_settings/__init__.py
+++ mountainash_settings/__init__.py
@@ -1,3 +1,7 @@
 from .version import __version__
 
 
+from .settings import SettingsParameters
+from .auth_settings import AuthSettings, get_auth_settings
+
+__all__ = ["SettingsParameters", "AuthSettings", "get_auth_settings"]
--- mountainash_settings/app_settings/__init__.py
+++
@@ -1,8 +0,0 @@
-from mountainash_settings.app_settings.app_settings import AppSettings
-from mountainash_settings.app_settings.settings_functions import get_app_settings
-
-__all__ = (
-    "AppSettings",
-    "get_app_settings"
-)
-
