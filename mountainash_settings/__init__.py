625a6b4603806e2c794f299805941bb8812411ab
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-03-20 17:11:31 +1100

    added get_auth_settings function to init

--- mountainash_settings/__init__.py
+++ mountainash_settings/__init__.py
@@ -2,7 +2,7 @@ from .version import __version__
 
 
 from .settings import SettingsParameters
-from .auth_settings import AuthSettings, get_auth_settings
+from .auth_settings import AuthSettings, get_auth_settings, prepare_auth_settings_parameters
 from .settings.settings_utils import SettingsUtils
 
-__all__ = ["SettingsParameters", "AuthSettings", "get_auth_settings", "SettingsUtils"]
+__all__ = ["SettingsParameters", "AuthSettings", "get_auth_settings", "SettingsUtils", "prepare_auth_settings_parameters"]
--- mountainash_settings/auth_settings/__init__.py
+++ mountainash_settings/auth_settings/__init__.py
@@ -1,8 +1,9 @@
 from mountainash_settings.auth_settings.auth_settings import AuthSettings
-from mountainash_settings.auth_settings.settings_functions import get_auth_settings
+from mountainash_settings.auth_settings.settings_functions import get_auth_settings, prepare_auth_settings_parameters
 
 __all__ = (
     "AuthSettings",
-    "get_auth_settings"
+    "get_auth_settings",
+    "prepare_auth_settings_parameters"
 )
 
